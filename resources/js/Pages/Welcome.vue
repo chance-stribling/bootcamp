7f8a17ceb735e9f4476ed594a69e1e731e6089eb
chance-stribling <cstribling1717@gmail.com>
2023-08-24 10:51:54 -0400

    first laravel vue project

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Hey" />
+
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-green-100  dark:bg-green-900 selection:bg-blue-500 selection:text-white"
+    >
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                >Dashboard</Link
+            >
+
+            <template v-else>
+                <Link
+                    :href="route('login')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    >Log in</Link
+                >
+
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    >Register</Link
+                >
+            </template>
+        </div>
+        <h1 class="font-semibold">Hello World</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
